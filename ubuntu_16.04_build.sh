b831704a9c667c2d43c4af2454f3e0e052ab5b7d
Tobias Wilson <tw315@kent.ac.uk>
2017-10-30 13:05:53 +0000

    Started script

--- ubuntu_16.04_build.sh
+++ ubuntu_16.04_build.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+
+# ============================= #
+#   Designed for Ubuntu 16.04 
+# To build your server for you 
+# ============================= #
+#
+#
+#
+# ============================= #
+# Input your system details here
+# ============================= #
+#
+
+HOSTNAME =
+SYSTEMIP = 
+DOMAIN = 
+LANGUAGE = 
+CHARSTET = 
+SSHPORT = 
+USER = 
+ADMINEMAIL = 
+PUBLICKEY = 
+
+# ============================= #
+#      End of manual input
+# ============================= #
 
